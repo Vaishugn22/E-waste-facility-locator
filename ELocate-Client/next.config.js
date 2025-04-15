1ab4c1753f95a80e233b591697c397b8df0be3bf
Vaishugn22 <117620989+Vaishugn22@users.noreply.github.com>
2025-04-15 17:13:46 +0530

    Add files via upload

---
+++ ELocate-Client/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false, // Optional: You can add this for best practices
+  images: {
+    domains: ['avatars.githubusercontent.com'], 
+  },
+};
+
+module.exports = nextConfig;
